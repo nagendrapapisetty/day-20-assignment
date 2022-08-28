09d443a2890c5ccbf4f0811f3c458dee75ca1956
unknown <papisettynagendra>
2022-08-28 18:59:57 +0530

    added  EMAIL

--- test/java/com/bridgelabz/UserRegistrationTest.java
+++ test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,13 +1,24 @@
 package com.bridgelabz;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class UserRegistrationTest {
     @Test
     public void testFirstName() throws UserRegistrationException {
         assertTrue(UserRegistration.firstName("nagendra"));
-        assertFalse(UserRegistration.firstName("papisetty"));
+        assertFalse(UserRegistration.firstName("fkdfmek"));
     }
-}
+    @Test
+    public void testLastName() throws UserRegistrationException {
+        assertTrue(UserRegistration.lastName("papisetty"));
+        assertFalse(UserRegistration.lastName("ahhmlm"));
+    }
+    @Test
+    public void testEmail() throws UserRegistrationException {
+        assertTrue(UserRegistration.email("nagendrapapisetty52@gamil.com"));
+        assertFalse(UserRegistration.email("nagendrapapisetty.u,123"));
 
+    }
 }
