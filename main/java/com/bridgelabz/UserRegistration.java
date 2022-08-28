113d50eddc1059e427be55b511fb29dc30a713ee
unknown <papisettynagendra>
2022-08-28 20:00:28 +0530

    added last name

--- main/java/com/bridgelabz/UserRegistration.java
+++ main/java/com/bridgelabz/UserRegistration.java
@@ -4,17 +4,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static boolean firstName(String LastName) throws UserRegistrationException {
-        if (LastName == null) {
+    public static boolean firstName(String firstName) throws UserRegistrationException {
+        if (firstName == null) {
             throw new UserRegistrationException(UserRegistrationException.ExceptionType.NULL, " Input can't be Null");
-        } else if (LastName.length() == 0) {
+        } else if (firstName.length() == 0) {
             throw new UserRegistrationException(UserRegistrationException.ExceptionType.EMPTY, "Invalid Input");
         }
         {
             String regex = "^[A-Z]{1}[a-z]{5,}$";
             Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(LastName);
+            Matcher matcher = pattern.matcher(firstName);
             return matcher.matches();
         }
+    }
+    public static boolean lastName(String lastName) throws UserRegistrationException {
+        if (lastName == null) {
+            throw new UserRegistrationException(UserRegistrationException.ExceptionType.NULL, " Input can't be Null");
+        } else if (lastName.length() == 0) {
+            throw new UserRegistrationException(UserRegistrationException.ExceptionType.EMPTY, "Invalid Input");
+        }
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
     }
 }
--- test/java/com/bridgelabz/UserRegistrationTest.java
+++ test/java/com/bridgelabz/UserRegistrationTest.java
@@ -3,11 +3,16 @@ package com.bridgelabz;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class UserRegistrationTest {
+
     @Test
     public void testFirstName() throws UserRegistrationException {
-        assertTrue(UserRegistration.firstName("nagendra"));
-        assertFalse(UserRegistration.firstName("papisetty"));
+        assertTrue(UserRegistration.firstName("Nagendra"));
+        assertFalse(UserRegistration.firstName("fkdfmek"));
+    }
+    @Test
+    public void testLastName() throws UserRegistrationException {
+        assertTrue(UserRegistration.lastName("Papisetty"));
+        assertFalse(UserRegistration.lastName("ahhmlm"));
     }
 }
 
-}
